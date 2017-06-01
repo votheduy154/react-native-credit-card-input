124a04e873ffc39d6996c7e2293c609c6898be84
DuyNguyen <duynguyen@DuyNguyens-Mac-mini.local>
2017-06-01 09:39:46 +0700

    no message

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -161,7 +161,7 @@ export default class CreditCardInput extends Component {
             scrollEnabled={allowScroll}
             showsHorizontalScrollIndicator={false}
             style={s.form}>
-          <CCInput {...this._inputProps("number")}
+          <CCInput additionalInputProps={{ maxLength: 19 }} {...this._inputProps("number")}
               containerStyle={[s.inputContainer, inputContainerStyle, { width: CARD_NUMBER_INPUT_WIDTH }]} />
           <CCInput {...this._inputProps("expiry")}
               containerStyle={[s.inputContainer, inputContainerStyle, { width: EXPIRY_INPUT_WIDTH }]} />
